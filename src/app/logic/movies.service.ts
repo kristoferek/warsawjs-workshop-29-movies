467fc4cecd1c70ad550650feed7f38dd7ea12a3f
Krzysztof Nowak <krzysztof.michal.nowak@gmail.com>
2019-02-24 15:04:27 +0100

    feature fetching environment for git

--- src/app/logic/movies.service.ts
+++ src/app/logic/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class MoviesService {
@@ -7,7 +8,7 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   async fetchAsyncMovies() {
-      const promise = await this.http.get('/assets/movies.json').toPromise();
+      const promise = await this.http.get(environment.moviesUrl).toPromise();
       return promise;
   }
 }
