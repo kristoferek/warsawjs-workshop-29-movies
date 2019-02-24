7b5a300c871022897a46044ecb22fea3920cfa3a
Krzysztof Nowak <krzysztof.michal.nowak@gmail.com>
2019-02-24 11:19:44 +0100

    fix path to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styles: [],
-  
+  templateUrl: './app.component.html'
+
 })
 export class AppComponent {
   title = 'movies';
