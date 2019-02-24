fce22bf12ee8c76e1210ca34991d9b3b3628a7bc
Krzysztof Nowak <krzysztof.michal.nowak@gmail.com>
2019-02-24 11:57:44 +0100

    feature menu module importing

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'airbnb',
+
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    rules: {},
+    globals: {},
+    parserOptions: {}
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundColorDirective } from './background-color.directive';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundColorDirective
+    BackgroundColorDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
